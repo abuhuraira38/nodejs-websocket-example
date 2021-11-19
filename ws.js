fa34fc99c2cdb86499295b8e876f9d6a66346aea
abuhuraira38 <93987240+abuhuraira38@users.noreply.github.com>
2021-11-19 11:18:49 +0500

    Update ws.js

--- ws.js
+++ ws.js
@@ -1,5 +1,21 @@
-var WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({port: 3000})
+// Minimal amount of secure websocket server
+var fs = require('fs')
+
+// read ssl certificate
+var privateKey = fs.readFileSync('key.pem', 'utf8')
+var certificate = fs.readFileSync('server.crt', 'utf8')
+
+var credentials = { key: privateKey, cert: certificate }
+var https = require('https')
+
+//pass in your credentials to create an https server
+var httpsServer = https.createServer(credentials)
+httpsServer.listen(3000)
+
+var WebSocketServer = require('ws').Server
+var wss = new WebSocketServer({
+  server: httpsServer
+})
 
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
