7db2afbc2170f8af3ed914e6dfe55b994aabd5ac
abuhuraira38 <93987240+abuhuraira38@users.noreply.github.com>
2021-11-19 13:55:17 +0500

    Update ws.js

--- ws.js
+++ ws.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 
 // read ssl certificate
 var privateKey = fs.readFileSync('key.pem', 'utf8')
-var certificate = fs.readFileSync('server.crt', 'utf8')
+var certificate = fs.readFileSync('cert.pem', 'utf8')
 
 var credentials = { key: privateKey, cert: certificate }
 var https = require('https')
